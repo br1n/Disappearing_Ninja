90492d28137bdd926b28fa237cd99a9662338a48
Brett <frettbishel@gmail.com>
2018-08-23 18:36:49 -0500

    thank you can i have some more

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninja')
+def ninja():
+    return render_template("ninja.html", img='tmnt.png')
+
+@app.route('/ninja/<color>')
+def ninja_color(color):
+    if color == "blue":
+        img = "leonardo.jpg"
+    
+    elif color == "red":
+        img = "raphael.jpg"
+    
+    elif color == "orange":
+        img = "michelangelo.jpg"
+
+    elif color == "purple":
+        img = "donatello.jpg"
+
+    else:
+        img = "notapril.jpg"
+    
+    return render_template('ninja.html', img=img)
+
+app.run(debug=True)
